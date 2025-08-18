1ad5191873aeaa2c3cd3cf5ef2a698661eb729e7
Carol <carolinesuisso95@gmail.com>
2025-08-18 19:40:30 -0300

    Adiciona script calculadora numeros inteiros :D

---
+++ pratica01/calculadora_numero_int.py
@@ -0,0 +1,12 @@
+#numeros inteiros
+A = int(input("Valor de A: "))
+B = int(input("Valor de B: "))
+C = int(input("Valor de C: "))
+D = int(input("Valor de D: "))
+
+diferenca = (A * B - C * D) 
+
+print(f"DIFERENCA= {diferenca}")
+
+
+
