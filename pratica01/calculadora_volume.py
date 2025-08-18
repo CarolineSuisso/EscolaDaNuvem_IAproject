9ba58422ca33d57cf041fbff3a753cd199cae292
Carol <carolinesuisso95@gmail.com>
2025-08-18 19:23:16 -0300

    Adicionar script da calculadora de volume

---
+++ pratica01/calculadora_volume.py
@@ -0,0 +1,9 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * altura * largura
+
+
+print(f"As dimensôes da caixa são {comprimento}cm x {largura}cm x {altura}cm.")
+print(f"O volume da caixa é de {volume}cm³.")
